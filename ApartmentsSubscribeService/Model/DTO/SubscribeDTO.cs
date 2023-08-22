66ef66e0f9b5d34a220e53a5e9d0c0f309c6c3fc
Alex <foxandr98@mail.ru>
2023-08-23 00:59:17 +0500

    Класс для запросов на подписку с валидацией SubscribeDTO

---
+++ ApartmentsSubscribeService/Model/DTO/SubscribeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApartmentsSubscribeService.Model.DTO
+{
+    public class SubscribeDTO
+    {
+        [Required(ErrorMessage = "Email не может быть пустым!")]
+        [EmailAddress(ErrorMessage = "Ваш email имеет некорректный формат!")]
+        [MaxLength(100, ErrorMessage = "Длина email не может быть длинее 100 символов!")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "URL не может быть пустым!")]
+        [Url(ErrorMessage = "Ваш URL имеет некорректный формат!")]
+        [MaxLength(100, ErrorMessage = "Длина ссылки не может быть длинее 100 символов!")]
+        public string Url { get; set; } = null!;
+    }
+}
